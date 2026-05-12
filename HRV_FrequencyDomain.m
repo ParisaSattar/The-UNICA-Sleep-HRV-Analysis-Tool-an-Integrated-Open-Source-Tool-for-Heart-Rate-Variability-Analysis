8d205981f05d3df204d7e38b7c7007a3b3046235
Parisa Sattar <54979828+ParisaSattar@users.noreply.github.com>
2026-05-12 08:16:58 +0200

    HRV_FrequencyDomain
    Updated frequency bands for Matlab 2025

--- HRV_FrequencyDomain.m
+++ HRV_FrequencyDomain.m
@@ -1,7 +1,7 @@
 function [aVLF, aLF, aHF, nLF,nHF,lfhf]=HRV_FrequencyDomain(RpkLoction,RR_intervals,fs)
 global Pxx F
 
-RR_index = (RpkLoction(1:end-1)-1)/fs;
+RR_index = RpkLoction(1:end-1)/fs;
 % Calculating time for RR intervals data interpolation 
 Fs_new=4;
 time=RR_index(1):1/Fs_new:RR_index(end);
@@ -9,8 +9,8 @@ time=time';
 % Interpolating RR intervals for frequency domain analysis
 RR_intervals=((RR_intervals)/fs)*1000;
 RR_intervals_Interpolated = interp1(RR_index,RR_intervals,time,'spline');
-RR_intervals_Interpolated=detrend(RR_intervals_Interpolated,'linear');
-RR_intervals_Interpolated=RR_intervals_Interpolated-mean(RR_intervals_Interpolated); %detrend is necessary to otain the trend 
+RR_intervals_Interpolated=detrend(RR_intervals_Interpolated,'linear'); %detrend is necessary to otain the trend 
+
 
 %for pwelch
 %w=length(RR_intervals_Interpolated)/2;
@@ -23,8 +23,8 @@ noverlap = round(w/2);
 
 % Defining band frequency
 VLF = [0.0033 0.04];
-LF = [0.046 0.158];
-HF = [0.158 0.400];
+LF = [0.04 0.15];
+HF = [0.15 0.4];
 
 
 % find the indexes corresponding to the VLF, LF, and HF bands
